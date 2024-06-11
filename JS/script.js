fd804b5c73d34457b0e546a9da52c6ea72243de0
Truong Minh Danh <151100650+MinhDanh2005@users.noreply.github.com>
2024-06-11 16:21:00 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,6 @@ var isFollowing = false;
 function toggleFollow() {
     var followBtn = document.getElementById('followBtn');
     isFollowing = !isFollowing;
-    followBtn.innerHTML = isFollowing ? 'Đã theo dõi ✔' : 'Nhấp để theo dõi';
+    followBtn.innerHTML = isFollowing ? 'Đã theo dõi ✔' : 'Theo dõi';
     followBtn.style.backgroundColor = isFollowing ? '#4CAF50' : '#f44336';
 }
